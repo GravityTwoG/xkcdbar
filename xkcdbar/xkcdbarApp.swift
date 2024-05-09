beb7ff582a7649743ed590d1e3cd6c85424ed9a9
GravityTwoG <crytekov@gmail.com>
2024-05-09 13:15:47 +0700

    Initial Commit

---
+++ xkcdbar/xkcdbarApp.swift
@@ -0,0 +1,17 @@
+//
+//  xkcdbarApp.swift
+//  xkcdbar
+//
+//  Created by Marsel Abazbekov on 09.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct xkcdbarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
